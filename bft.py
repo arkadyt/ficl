a953e9cec24835b7e4ca0b3b16a51d7fadaa3fd1
arkadyt <contact.me@arkadyt.dev>
2018-05-04 22:59:09 -0700

    Improved verbose output, added report of ignored and locked files.

--- bft.py
+++ bft.py
@@ -60,23 +60,31 @@ def clean(args):
 
     filelist = list_files(args.path, args.recursively)
     successfully_cleaned = len(filelist)
+    special_files = 0
+    could_not_delete = 0
 
     for filepath in filelist:
         if os.path.basename(filepath)[0] == '.' and args.no_special_files:
             # Stumbled onto special file starting with a dot and --no-special-files flag
             # was set.
             successfully_cleaned -= 1
+            special_files += 1
         else:
             try:
                 with open(filepath, 'w') as f:
                     f.write('')
             except PermissionError:
                 successfully_cleaned -= 1
+                could_not_delete += 1
                 if args.verbosity:
                     print('Could not clean out {}. Permission denied.'.format(filepath))
 
     if args.verbosity:
         print(f'Successfully cleaned out {successfully_cleaned}/{len(filelist)} files.')
+        if args.no_special_files:
+            print(f'{special_files} special files were ignored.')
+        if could_not_delete > 0:
+            print(f'Failed to delete {could_not_delete} files.')
 
     if args.prefix is not None or args.postfix is not None:
         # User needs to rename files too.
@@ -94,12 +102,15 @@ def rename(args):
 
     filelist = list_files(args.path, args.recursively)
     successfully_renamed = len(filelist)
+    special_files = 0
+    could_not_delete = 0
 
     for filepath in filelist:
-        if os.path.basename(filepath)[0] == '.' and args.no_spec_files:
+        if os.path.basename(filepath)[0] == '.' and args.no_special_files:
             # Stumbled onto special file starting with a dot and --no-special-files flag
             # was set.
             successfully_renamed -= 1
+            special_files += 1
         else:
             try:
                 new_name = os.path.join(
@@ -109,11 +120,16 @@ def rename(args):
                 os.rename(filepath, new_name)
             except PermissionError:
                 successfully_renamed -= 1
+                could_not_delete += 1
                 if args.verbosity:
                     print('Could not rename {}. Permission denied.'.format(filepath))
     
     if args.verbosity:
         print(f'Successfully renamed {successfully_renamed}/{len(filelist)} files.')
+        if args.no_special_files:
+            print(f'{special_files} special files were ignored.')
+        if could_not_delete > 0:
+            print(f'Failed to rename {could_not_delete} files.')
 
 
 if __name__ == '__main__':
